8c4d6c1a672f2e6ab4e632afb298441d88169bcf
pradeep sn <pradeepsn606@gmail.com>
2023-12-07 01:03:32 +0530

    readme

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
-import Image from "next/image";
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import classes from "./Main.module.css";
+import GraphCard from "@/components/graphCard/graphCard";
+import ResultCard from "@/components/resultCard/resultCard";
+function Main() {
+  const [data, setData] = useState([]);
+  const [sliderValue, setSliderValue] = useState(1);
+  const [currentBtn, setCurrentBtn] = useState(-1);
+  const [submitData, setSubmitData] = useState({ xParam: 0, yParam: 0 });
+  const [bookTitles, setBookTitles] = useState([
+    "Submit the form to view Results",
+  ]);
 
-export default function Home() {
+  const genreTitles = ["Action", "Fantasy", "Thriller"];
+  function changeCurrentBtn(num: number) {
+    setCurrentBtn(num);
+  }
+  function handleFormSubmit(event: any) {
+    event.preventDefault();
+    setSubmitData({ xParam: currentBtn, yParam: sliderValue });
+    window.scrollTo({ top: 600, behavior: "smooth" });
+  }
+
+  useEffect(() => {
+    console.log(submitData);
+    if (submitData) {
+      fetch("http://localhost:8080/getBooks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(submitData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data.bookValues);
+          setBookTitles(data.bookNames);
+        });
+    }
+  }, [submitData]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={classes.bg}>
+      <div className={classes.navbar}>
+        <h2>Book Selector</h2>
+      </div>
+
+      <div className={classes.contentCards}>
+        <div className={classes.formCard}>
+          <form>
+            <p className={classes.genreQn}> Select a Genre you prefer</p>
+            <div className={classes.genreButtons}>
+              {genreTitles.map((value, index) => {
+                return (
+                  <button
+                    key={index}
+                    className={classes.genreButton}
+                    type="button"
+                    style={{
+                      backgroundColor: `${
+                        index + 1 === currentBtn ? "lightBlue" : "lightGreen"
+                      }`,
+                    }}
+                    onClick={changeCurrentBtn.bind(null, index + 1)}
+                  >
+                    {value}
+                  </button>
+                );
+              })}
+            </div>
+            <div className={classes.formGroup}>
+              <p className={classes.genreQn}>
+                On a scale of 1-10, How Big do you want your book to be ?
+              </p>
+              <div className={classes.sliderGroup}>
+                <input
+                  type="range"
+                  id="slider"
+                  name="slider"
+                  min="1"
+                  max="10"
+                  value={sliderValue}
+                  onChange={(event) =>
+                    setSliderValue(Number.parseInt(event.target.value))
+                  }
+                />
+                <div className={classes.sliderVal}>{sliderValue}</div>
+              </div>
+            </div>
+            <div className={classes.formGroup}>
+              <button
+                type="submit"
+                className={classes.submitButton}
+                onClick={handleFormSubmit}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <ResultCard bookNames={bookTitles}></ResultCard>
+      <GraphCard
+        studentVector={[submitData.xParam, submitData.yParam]}
+        bookVector={data}
+        bookNames={bookTitles}
+      ></GraphCard>
+    </div>
   );
 }
+export default Main;
